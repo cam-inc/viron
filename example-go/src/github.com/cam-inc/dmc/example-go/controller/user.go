daca5c488fb3a346cf1f895ed7de910433184dd4
Noritaka Okada <nori0123@gmail.com>
2017-04-10 20:16:07 +0900

    rename

---
+++ example-go/src/github.com/cam-inc/dmc/example-go/controller/user.go
@@ -0,0 +1,70 @@
+package controller
+
+import (
+	"github.com/cam-inc/dmc/example-go/gen/app"
+	"github.com/goadesign/goa"
+)
+
+// UserController implements the user resource.
+type UserController struct {
+	*goa.Controller
+}
+
+// NewUserController creates a user controller.
+func NewUserController(service *goa.Service) *UserController {
+	return &UserController{Controller: service.NewController("UserController")}
+}
+
+// Create runs the create action.
+func (c *UserController) Create(ctx *app.CreateUserContext) error {
+	// UserController_Create: start_implement
+
+	// Put your logic here
+
+	// UserController_Create: end_implement
+	res := &app.User{}
+	return ctx.OK(res)
+}
+
+// Delete runs the delete action.
+func (c *UserController) Delete(ctx *app.DeleteUserContext) error {
+	// UserController_Delete: start_implement
+
+	// Put your logic here
+
+	// UserController_Delete: end_implement
+	return nil
+}
+
+// List runs the list action.
+func (c *UserController) List(ctx *app.ListUserContext) error {
+	// UserController_List: start_implement
+
+	// Put your logic here
+
+	// UserController_List: end_implement
+	res := app.UserCollection{}
+	return ctx.OK(res)
+}
+
+// Show runs the show action.
+func (c *UserController) Show(ctx *app.ShowUserContext) error {
+	// UserController_Show: start_implement
+
+	// Put your logic here
+
+	// UserController_Show: end_implement
+	res := &app.User{}
+	return ctx.OK(res)
+}
+
+// Update runs the update action.
+func (c *UserController) Update(ctx *app.UpdateUserContext) error {
+	// UserController_Update: start_implement
+
+	// Put your logic here
+
+	// UserController_Update: end_implement
+	res := &app.User{}
+	return ctx.OK(res)
+}
---
+++ example-go/src/github.com/cam-inc/dmc/example-go/gen/tool/example-go-cli/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cam-inc/dmc/example-go/gen/client"
+	"github.com/cam-inc/dmc/example-go/gen/tool/cli"
+	goaclient "github.com/goadesign/goa/client"
+	"github.com/spf13/cobra"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	// Create command line parser
+	app := &cobra.Command{
+		Use:   "example-go-cli",
+		Short: `CLI client for the example-go service`,
+	}
+
+	// Create client struct
+	httpClient := newHTTPClient()
+	c := client.New(goaclient.HTTPClientDoer(httpClient))
+
+	// Register global flags
+	app.PersistentFlags().StringVarP(&c.Scheme, "scheme", "s", "", "Set the requests scheme")
+	app.PersistentFlags().StringVarP(&c.Host, "host", "H", "localhost:3000", "API hostname")
+	app.PersistentFlags().DurationVarP(&httpClient.Timeout, "timeout", "t", time.Duration(20)*time.Second, "Set the request timeout")
+	app.PersistentFlags().BoolVar(&c.Dump, "dump", false, "Dump HTTP request and response.")
+
+	// Initialize API client
+	c.UserAgent = "example-go-cli/0.0.1"
+
+	// Register API commands
+	cli.RegisterCommands(app, c)
+
+	// Execute!
+	if err := app.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(-1)
+	}
+}
+
+// newHTTPClient returns the HTTP client used by the API client to make requests to the service.
+func newHTTPClient() *http.Client {
+	// TBD: Change as needed (e.g. to use a different transport to control redirection policy or
+	// disable cert validation or...)
+	return http.DefaultClient
+}
