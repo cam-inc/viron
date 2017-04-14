a4c9b9178cc899ecc313be4d8a6bc29dc898b419
Noritaka Okada <nori0123@gmail.com>
2017-04-14 18:35:02 +0900

    Fix. settings

--- example-go/src/github.com/cam-inc/dmc/example-go/common/db.go
+++ example-go/src/github.com/cam-inc/dmc/example-go/common/db.go
@@ -6,13 +6,21 @@ import (
 	"github.com/cam-inc/dmc/example-go/gen/models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 )
 
 var DB *gorm.DB
 
 func getConnectionString() string {
-	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s?parseTime=true",
-		"user", "password", "tcp", "mysql", "3306", "dmc_local")
+	service := os.Getenv("SERVICE_ENV")
+	if service == "docker-local" {
+		// on docker
+		return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s?parseTime=true",
+			"user", "password", "tcp", "mysql", "3306", "dmc_local")
+	} else {
+		return fmt.Sprintf("%s:%s@%s(%s:%s)/%s?parseTime=true",
+			"user", "password", "tcp", "localhost", "3306", "dmc_local")
+	}
 }
 
 func InitDB() {
