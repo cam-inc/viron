ca6d4c16ead0ddb76de1c6491fe9b6c7970730b6
fkei <kei.topaz@gmail.com>
2017-04-24 20:52:28 +0900

    Added: stats dau/mau api

---
+++ example-go/src/github.com/cam-inc/dmc/example-go/controller/stats_dau.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"github.com/cam-inc/dmc/example-go/gen/app"
+	"github.com/goadesign/goa"
+)
+
+// StatsDauController implements the stats_dau resource.
+type StatsDauController struct {
+	*goa.Controller
+}
+
+// NewStatsDauController creates a stats_dau controller.
+func NewStatsDauController(service *goa.Service) *StatsDauController {
+	return &StatsDauController{Controller: service.NewController("StatsDauController")}
+}
+
+// Show runs the show action.
+func (c *StatsDauController) Show(ctx *app.ShowStatsDauContext) error {
+	// StatsDauController_Show: start_implement
+
+	// Put your logic here
+
+	// StatsDauController_Show: end_implement
+	res := &app.Statsdau{
+		Value: 1234567,
+	}
+	return ctx.OK(res)
+}
