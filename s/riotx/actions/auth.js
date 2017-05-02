78a9020b679dbc224ee7c89e85634595e3794c24
fkei <kei.topaz@gmail.com>
2017-05-02 11:36:54 +0900

    Added: tmp oauth google

--- s/riotx/actions/auth.js
+++ s/riotx/actions/auth.js
@@ -4,7 +4,7 @@ export default {
   update: (context, key) => {
     const endpoint = context.getter(constants.GETTER_ENDPOINT_ONE, key);
     return fetch(endpoint.url, {
-      headers: { "Authorization": `Bearer: ${endpoint.token}` }
+      headers: {"Authorization": `Bearer: ${endpoint.token}`}
     })
       .then((response) => {
         if (response.status === 401) {
@@ -13,7 +13,27 @@ export default {
         }
         context.commit(constants.MUTATION_ENDPOINT_TOKEN_UPDATE, key, endpoint.token);
       })
-    ;
+      ;
+  },
+
+  //
+  signInGoogle: (context, key, authtype) => {
+    const endpoint = context.getter(constants.GETTER_ENDPOINT_ONE, key);
+    const url = new URL(endpoint.url);
+    let fetchUrl = `${url.origin}/${authtype.url}`;
+    return fetch(fetchUrl, {
+      method: 'POST',
+      mode: 'cors',
+    })
+      .then((response) => {
+        debugger;
+        if (response.status === 401) {
+          context.commit(constants.MUTATION_ENDPOINT_TOKEN_UPDATE, key, null);
+          return;
+        }
+        context.commit(constants.MUTATION_ENDPOINT_TOKEN_UPDATE, key, endpoint.token);
+      })
+      ;
   },
 
 }
