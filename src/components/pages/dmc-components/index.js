f904d6e7d0501060baf6d09946a3101a8bf5eada
Go Ohtani <cathcheeno@gmail.com>
2017-07-27 16:32:52 +0900

    Fixed: re-render when grid column size got changed.

--- src/components/pages/dmc-components/index.js
+++ src/components/pages/dmc-components/index.js
@@ -1,4 +1,5 @@
 import throttle from 'mout/function/throttle';
+import chart from '../../../core/chart';
 import { constants as getters } from '../../../store/getters';
 import { constants as states } from '../../../store/states';
 
@@ -11,8 +12,15 @@ export default function() {
   // resizeイベントハンドラーの発火回数を減らす。
   const updateGridColumnCount = throttle(() => {
     const containerWidth = this.refs.list.getBoundingClientRect().width;
+    const htmlStyles = window.getComputedStyle(document.querySelector('html'));
+    const currentColumnCount = Number(htmlStyles.getPropertyValue('--page-components-grid-column-count'));
     const newColumnCount = Math.floor(containerWidth / 250) || 1;
     document.documentElement.style.setProperty('--page-components-grid-column-count', newColumnCount);
+    // tauchartはresize時に自動で再レンダリングするが、column数変更時にはresizeイベントが発火しないため再レンダリングが実行されない。
+    // column数変更時も再レンダリングさせるために手動でresizeイベントハンドラを実行する。
+    if (currentColumnCount !== newColumnCount) {
+      chart.Chart.resizeOnWindowEvent();
+    }
   }, 1000);
 
   this.on('mount', () => {
