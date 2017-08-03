6b81b6b4e758a281b8521a743b3d8321d148ac8b
大村 豪 <y41142@NB-733.local>
2017-08-03 15:53:06 +0900

    Added: user_blog create

--- example-node/api/controllers/user_blog.js
+++ example-node/api/controllers/user_blog.js
@@ -27,9 +27,7 @@ const list = (req, res) => {
         attributes,
         limit,
         offset,
-        order: [['createdAt', 'DESC']],
       };
-      console.log(options)
       return UserBlogs.findAll(options);
     })
     .then(list => {
@@ -38,6 +36,34 @@ const list = (req, res) => {
     ;
 };
 
+/**
+ * Controller : Create  User Blog
+ * HTTP Method : POST
+ * PATH : /userblog
+ *
+ * @returns {function(*, *)}
+ */
+
+const create = (req, res) => {
+  const store = shared.context.getStoreMain();
+  const UserBlogs = store.models.UserBlogs;
+  return Promise.resolve()
+    .then(() => {
+      var data = new Object();
+      data.user_id = req.body.user_id;
+      data.title = req.body.title;
+      data.subtitle = req.body.subtitle;
+      data.genre = req.body.genre;
+      data.design_id = req.body.design_id;
+      return UserBlogs.create(data);
+    })
+    .then(data => {
+      res.json(data);
+    })
+    ;
+}
+
 module.exports = {
   'user_blog#list': list,
+  'user_blog#create': create,
 };
