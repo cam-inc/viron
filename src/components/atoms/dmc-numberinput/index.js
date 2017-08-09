130bca3090e789012d5b86b260133d393df29394
tosaka07 <tosaka.tech@gmail.com>
2017-08-09 22:25:30 +0900

    Updated: implemented the Numberinput component.

--- src/components/atoms/dmc-numberinput/index.js
+++ src/components/atoms/dmc-numberinput/index.js
@@ -1,3 +1,57 @@
 export default function() {
-  
+
+  this.handleIncreaseButtonPat = () => {
+    let number = this.opts.number;
+    // numberが空文字/null/undefined/0
+    if (!number) {
+      number = 0;
+    }
+    if (number === '-') {
+      number = 0;
+    }
+    this.opts.onchange(number + (this.opts.step || 1) );
+  };
+
+  this.handleDecreaseButtonPat = () => {
+    let number = this.opts.number;
+    // numberが空文字/null/undefined/0
+    if (!number) {
+      number = 0;
+    }
+    if (number === '-') {
+      number = 0;
+    }
+    this.opts.onchange(number - (this.opts.step || 1) );
+  };
+
+  this.handleKeyDown = (e) => {
+    switch (e.code) {
+    case 'ArrowUp':
+      this.handleIncreaseButtonPat();
+      break;
+    case 'ArrowDown':
+      this.handleDecreaseButtonPat();
+      break;
+    }
+  };
+
+  // @see: https://developer.mozilla.org/ja/docs/Web/API/Element/setAttribute
+  // setAttribute() を使ってある属性、XUL や HTML の特別な値、および HTML の選択領域の変更は、属性がデフォルト値を特定している場合に一貫性の無い動作となります。現在の値にアクセスしたり、変更したりするにはプロパティを使用すべきです。具体例として、 elt .setAttribute('value', val ) の代わりに elt .value を使用します。
+  this.on('updated', () => {
+    this.refs.input.value = this.opts.number;
+  });
+
+  this.handleInputInput = e => {
+    e.preventUpdate = true;
+    let newText = e.target.value.replace(/[^-^0-9]/g, '');// eslint-disable-line no-irregular-whitespace
+    if(!newText.length || newText.match(/^([-]?[1-9][0-9]*?|-|0)$/)) {
+      this.opts.onchange(newText);
+    }else {
+      this.refs.input.value = this.opts.number;
+    }
+  };
+
+  this.handleInputChange = e => {
+    e.stopPropagation();
+  };
 }
--- src/components/atoms/dmc-numberinput/index.tag
+++ src/components/atoms/dmc-numberinput/index.tag
@@ -1,7 +1,10 @@
 dmc-numberinput.Numberinput
   .Numberinput__label(if="{ !!opts.label }") { opts.label }
-  form.Numberinput__form(onSubmit="{ handleFormSubmit }")
-  input.Numberinput__input(onTap="handleInputTap" ref="touch" value="{ opts.value }" placeholder="{ opts.placeholder || '' }")
+  form.Numberinput__form(onSubmit="{ handleFormSubmit }" onkeydown="{ handleKeyDown }")
+    input.Numberinput__input(ref="input" value="{ opts.number || '' }" placeholder="{ opts.placeholder || '' }" onInput="{ handleInputInput }" onChange="{ handleInputChange }")
+    .Numberinput__handler
+      button.Numberinput__handler__button(onTap="handleIncreaseButtonPat" ref="touch" disabled="{opts.number >= opts.max}") ▲
+      button.Numberinput__handler__button(onTap="handleDecreaseButtonPat" ref="touch" disabled="{opts.number <= opts.min}") ▼
 
   script.
     import script from './index';
