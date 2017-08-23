2f04046d843bb0b0e8395777eaf34abbd5f9492f
Jung0 <awf.15.92@gmail.com>
2017-08-23 16:28:10 +0900

    Added: Add Partialtime component

---
+++ src/components/atoms/dmc-timepicker/partial.js
@@ -0,0 +1,52 @@
+import moment from 'moment';
+import times from 'mout/function/times';
+
+export default function() {
+
+  this.isShowTimepicker = false;
+  this.handleInputTime = () => {
+    this.isShowTimepicker = true;
+  };
+
+  this.generateHours = () => {
+    const MAX_DISPLAY_HOURS = 24;
+    const hours = [];
+    times(MAX_DISPLAY_HOURS, (i) => {
+      r = ("0" + i).slice(-2);
+      hours[i] = {
+        hour: r
+      }
+    });
+    return hours;
+  };
+
+  this.generateMinutes = () => {
+    const MAX_DISPLAY_MINUTES = 60;
+    const minutes = [];
+    times(MAX_DISPLAY_MINUTES, (i) => {
+      r = ("0" + i).slice(-2);
+      minutes[i] = {
+        minute: r
+      }
+    });
+    return minutes;
+  };
+
+  this.generateSeconds = () => {
+    const MAX_DISPLAY_SECONDS = 60;
+    const seconds = [];
+    times(MAX_DISPLAY_SECONDS, (i) => {
+      r = ("0" + i).slice(-2);
+      seconds[i] = {
+        second: r
+      }
+    });
+    return seconds;
+  };
+
+  let defaultTime = '00:00:00-09:00';
+
+  let setTime = moment().format(defaultTime, 'HH:mm:ss-ZZ');
+
+  this.displayTime = setTime;
+}
