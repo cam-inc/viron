416e9a80057008b68de34ac9571dbe277b4b386c
Jung0 <awf.15.92@gmail.com>
2017-08-24 16:39:35 +0900

    Added: Default Time

--- src/components/atoms/dmc-timepicker/index.js
+++ src/components/atoms/dmc-timepicker/index.js
@@ -1,2 +1,26 @@
+import moment from 'moment';
 export default function() {
+
+  this.isShowTimepicker = false;
+  this.handleInputTime = () => {
+    this.isShowTimepicker = true;
+  };
+
+  let defaultPartialTime = '00:00:00';
+
+  let defaultOffsetTime = '-00:00';
+
+  let partialTime = moment().format(defaultPartialTime, 'HH:mm:ss');
+
+  let offsetTime = moment().format(defaultOffsetTime, 'ZZ');
+
+  let defaultFullTime = defaultPartialTime + defaultOffsetTime;
+
+  let fullTime = moment().format(defaultFullTime, 'HH:mm:ss ZZ');
+
+  this.partialTime = partialTime;
+
+  this.offsetTime = offsetTime;
+
+  this.fullTime = fullTime;
 }
