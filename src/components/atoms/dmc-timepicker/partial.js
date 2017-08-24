959dc8aa5c146303ec2ede7873a4d466899523cd
Jung0 <awf.15.92@gmail.com>
2017-08-24 16:43:41 +0900

    Added: Function digitNum

--- src/components/atoms/dmc-timepicker/partial.js
+++ src/components/atoms/dmc-timepicker/partial.js
@@ -2,19 +2,30 @@ import moment from 'moment';
 import times from 'mout/function/times';
 
 export default function() {
+  // let defaultTime = '00:00:00-09:00';
+  // let setTime = moment().format(defaultTime, 'HH:mm:ss-ZZ');
+  // this.displayTime = setTime;
 
-  this.isShowTimepicker = false;
-  this.handleInputTime = () => {
-    this.isShowTimepicker = true;
+  this.selectedDate = moment(this.opts.partialtime || null);
+
+  const format = date => {
+    return date.format('MM:hh:ss');
+  };
+
+  const digitNum = num => {
+    return num = ('0' + num).slice(-2);
   };
 
   this.generateHours = () => {
     const MAX_DISPLAY_HOURS = 24;
     const hours = [];
+
     times(MAX_DISPLAY_HOURS, (i) => {
-      let r = ('0' + i).slice(-2);
+      const date = '';
+      const displayNum = digitNum(i);
       hours[i] = {
-        hour: r
+        'date': date,
+        'hour': displayNum,
       };
     });
     return hours;
@@ -24,9 +35,11 @@ export default function() {
     const MAX_DISPLAY_MINUTES = 60;
     const minutes = [];
     times(MAX_DISPLAY_MINUTES, (i) => {
-      let r = ('0' + i).slice(-2);
+      const displayNum = digitNum(i);
+      const date = '';
       minutes[i] = {
-        minute: r
+        'date': date,
+        'minute': displayNum
       };
     });
     return minutes;
@@ -36,17 +49,23 @@ export default function() {
     const MAX_DISPLAY_SECONDS = 60;
     const seconds = [];
     times(MAX_DISPLAY_SECONDS, (i) => {
-      let r = ('0' + i).slice(-2);
+      const displayNum = digitNum(i);
+      const date = '';
       seconds[i] = {
-        second: r
+        'date': date,
+        'second': displayNum
       };
     });
     return seconds;
   };
 
-  let defaultTime = '00:00:00-09:00';
-
-  let setTime = moment().format(defaultTime, 'HH:mm:ss-ZZ');
-
-  this.displayTime = setTime;
+  this.handleSelectHour = () => {
+    console.log('hoge1');
+  };
+  this.handleSelectMinute = () => {
+    console.log('hoge2');
+  };
+  this.handleSelectSecond = () => {
+    console.log('hoge3');
+  };
 }
