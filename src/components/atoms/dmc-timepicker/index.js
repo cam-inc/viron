46eea740836fdb58b4cf69143c24345868cddc91
Jung0 <awf.15.92@gmail.com>
2017-08-28 15:12:57 +0900

    Added: For component integration

--- src/components/atoms/dmc-timepicker/index.js
+++ src/components/atoms/dmc-timepicker/index.js
@@ -1,4 +1,6 @@
 import moment from 'moment';
+import times from 'mout/function/times';
+
 export default function() {
 
   this.isShowTimepicker = false;
@@ -6,25 +8,126 @@ export default function() {
     this.isShowTimepicker = true;
   };
 
-  this.partialTimeHour = '';
-  this.partialTimeMinute = '';
-  this.partialTimeSecond ='';
+  const format = date => {
+    return date.format('HH:mm:ss');
+  };
 
-  let defaultPartialTime = '00:00:00';
+  const digitNum = num => {
+    return num = ('0' + num).slice(-2);
+  };
 
-  let defaultOffsetTime = '-00:00';
+  const space = ' ';
+  const colon = ':';
 
-  let partialTime = moment().format(defaultPartialTime, 'HH:mm:ss');
+  this.defaultPartialTime = this.opts.date;
 
-  let offsetTime = moment().format(defaultOffsetTime, 'ZZ');
+  const fulltimesplit = this.defaultPartialTime.split(space, 2);
+  var partialtime = fulltimesplit[0];
+  var offsettime = fulltimesplit[1];
 
-  let defaultFullTime = defaultPartialTime + defaultOffsetTime;
+  const partialtimesplit = partialtime.split(colon, 3);
+  var partialtimehour = partialtimesplit[0];
+  var partialtimeminute = partialtimesplit[1];
+  var partialtimesecond = partialtimesplit[2];
+  var partialtimehournum = Number(partialtimehour);
+  var partialtimeminutenum = Number(partialtimeminute);
+  var partialtimesecondnum = Number(partialtimesecond);
 
-  let fullTime = moment().format(defaultFullTime, 'HH:mm:ss ZZ');
+  //
+  //TODO: offsetがZのときの処理を追加する
+  //
+  if(offsettime === 'Z'){
 
-  this.partialTime = partialTime;
+  }else{
+    const offsettimesplit = offsettime.split(colon, 2);
+    var offsettimehour = offsettimesplit[0];
+    var offsettimeminute = offsettimesplit[1];
+    var offsettimehour = Number(offsettimehour);
+    var offsettimeminute = Number(offsettimeminute);
+  }
 
-  this.offsetTime = offsetTime;
+  //TODO:変数化
+  this.selectedItemHour = partialtimehournum;
+  this.selectedItemMinute = partialtimeminutenum;
+  this.selectedItemSecond = partialtimesecondnum;
 
-  this.fullTime = fullTime;
+  this.partialTime = `${this.selectedItemHour}:${this.selectedItemMinute}:${this.selectedItemSecond}`;
+
+  //TODO:変数化
+  this.displaySelectedItemHour = partialtimehour;
+  this.displaySelectedItemMinute = partialtimeminute;
+  this.displaySelectedItemSecond = partialtimesecond;
+
+  this.displayPartialTime = `${this.displaySelectedItemHour}:${this.displaySelectedItemMinute}:${this.displaySelectedItemSecond}`;
+
+  this.generateHours = () => {
+    const MAX_DISPLAY_HOURS = 24;
+    const hours = [];
+
+    times(MAX_DISPLAY_HOURS, i => {
+      // const date = this.defaultPartialTime.clone().add(i, 'hours');
+      const displayNum = digitNum(i);
+      hours[i] = {
+        'date': i,
+        'displayTime': displayNum,
+        // 'isSelected': format(date)
+      };
+    });
+    return hours;
+  };
+
+  this.generateMinutes = () => {
+    const MAX_DISPLAY_MINUTES = 60;
+    const minutes = [];
+    times(MAX_DISPLAY_MINUTES, i => {
+      const displayNum = digitNum(i);
+      minutes[i] = {
+        'date': i,
+        'displayTime': displayNum
+      };
+    });
+    return minutes;
+  };
+
+  this.generateSeconds = () => {
+    const MAX_DISPLAY_SECONDS = 60;
+    const seconds = [];
+    times(MAX_DISPLAY_SECONDS, i => {
+      const displayNum = digitNum(i);
+      seconds[i] = {
+        'date': i,
+        'displayTime': displayNum
+      };
+    });
+    return seconds;
+  };
+
+  this.on('update', () => {
+    this.partialTime = `${this.selectedItemHour}:${this.selectedItemMinute}:${this.selectedItemSecond}`;
+    this.testTime = `${this.selectedItemHour}:${this.selectedItemMinute}:${this.selectedItemSecond}`;
+    // console.log(this.testTime);
+    this.displayPartialTime = `${this.displaySelectedItemHour}:${this.displaySelectedItemMinute}:${this.displaySelectedItemSecond}`;
+  }).on('updated', () => {
+    this.rebindTouchEvents();
+  });
+
+  this.handleSelectHour = date => {
+    this.selectedItemHour = date;
+    console.log(this.selectedItemHour);
+    this.displaySelectedItemHour = digitNum(date);
+    this.update();
+  };
+
+  this.handleSelectMinute = date => {
+    this.selectedItemMinute = date;
+    this.displaySelectedItemMinute = digitNum(date);
+    this.update();
+  };
+
+  this.handleSelectSecond = date => {
+    this.selectedItemSecond = date;
+    this.displaySelectedItemSecond = digitNum(date);
+    this.update();
+  };
+
 }
