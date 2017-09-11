b9540f354dd5173d868003b643f98a9d9771ec29
Jung0 <awf.15.92@gmail.com>
2017-09-11 19:06:47 +0900

    Fixed: arrow-parens

--- src/core/oas.js
+++ src/core/oas.js
@@ -746,7 +746,7 @@ const format = (value, constraints) => {
       /^([0-9a-fA-F]{1,4}:){6}((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
     ];
     let matchResult = false;
-    forEach(patterns, (pattern) => {
+    forEach(patterns, pattern => {
       const isMatch = value.match(pattern);
       if (!isNull(isMatch)) {
         matchResult = true;
