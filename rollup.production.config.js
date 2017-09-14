327a13df295e92756f0095928e5d83fe85b890b8
Go Ohtani <cathcheeno@gmail.com>
2017-09-14 14:35:23 +0900

    Removed: unused rollup config file.

--- rollup.production.config.js
+++
@@ -1,8 +0,0 @@
-import ObjectAssign from 'object-assign';
-import baseConfig from './rollup.base.config.js';
-
-const config = ObjectAssign({}, baseConfig);
-
-// production specific settings here.
-
-export default config;
