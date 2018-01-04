f67fbe2b61f579f65d5be5031936d8964276a4a2
Go Ohtani <cathcheeno@gmail.com>
2018-01-04 12:18:47 +0900

    Added: function to release as beta version.

--- task/deploy.js
+++ task/deploy.js
@@ -1,10 +1,16 @@
-const version = require('../package.json').version;
+const commander = require('commander');
 const ghpages = require('gh-pages');
+const version = require('../package.json').version;
+
 const [major, minor, patch] = version.split('.');
+
+commander
+  .option('-b, --beta', 'On/Off flag of beta release.')
+  .parse(process.argv);
 
 new Promise((resolve, reject) => {
   const options = {
-    dest: `v${major}`,
+    dest: (commander.beta ? 'beta' : `v${major}`),
     add: true
   };
   ghpages.publish('dist', options, err => {
