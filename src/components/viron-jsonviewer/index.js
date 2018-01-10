a12aabc95b6c3722a70a9f94df6a0efb65e11f17
MuuKojima <muu.kojima.dev@gmail.com>
2018-01-10 20:37:10 +0900

    Fixed: js lint.

--- src/components/viron-jsonviewer/index.js
+++ src/components/viron-jsonviewer/index.js
@@ -9,61 +9,58 @@ export default function() {
     };
 
     const createItem = (key, value, type) => {
-        let element = TAG_TEMPLATES.item.replace('%KEY%', key);
-        if(type === 'string') {
-            element = element.replace('%VALUE%', '"' + value + '"');
-        } else {
-            element = element.replace('%VALUE%', value);
-        }
-        element = element.replace('%TYPE%', type);
-        return element;
-    }
+      let element = TAG_TEMPLATES.item.replace('%KEY%', key);
+      if (type === 'string') {
+        element = element.replace('%VALUE%', '"' + value + '"');
+      } else {
+        element = element.replace('%VALUE%', value);
+      }
+      element = element.replace('%TYPE%', type);
+      return element;
+    };
 
     const createCollapsibleItem = (key, value, type, children) => {
-        const tpl = collapsible ? 'itemCollapsibleOpen' : 'itemCollapsible';
-        let element = TAG_TEMPLATES[tpl].replace('%KEY%', key);
-        element = element.replace('%VALUE%', type);
-        element = element.replace('%TYPE%', type);
-        element = element.replace('%CHILDREN%', children);
-        return element;
-    }
+      const tpl = collapsible ? 'itemCollapsibleOpen' : 'itemCollapsible';
+      let element = TAG_TEMPLATES[tpl].replace('%KEY%', key);
+      element = element.replace('%VALUE%', type);
+      element = element.replace('%TYPE%', type);
+      element = element.replace('%CHILDREN%', children);
+      return element;
+    };
 
     const handleChildren = (key, value, type) => {
-        let children = '';
-        for(const item in value) {
-            const key = item
-            const val = value[key];
-            children += handleItem(key, val);
-        }
-        return createCollapsibleItem(key, value, type, children);
-    }
+      let children = '';
+      for (const item in value) {
+        const key = item;
+        const val = value[key];
+        children += handleItem(key, val);
+      }
+      return createCollapsibleItem(key, value, type, children);
+    };
 
     const handleItem = (key, value) => {
-        const type = typeof value;
-        if(value && typeof value === 'object') {
-            return handleChildren(key, value, type);
-        }
-        return createItem(key, value, type);
-    }
+      const type = typeof value;
+      if (value && typeof value === 'object') {
+        return handleChildren(key, value, type);
+      }
+      return createItem(key, value, type);
+    };
 
-    const parseObject = (responseJson) => {
-        let displayJson = '<div class="json">';
-        for(const item in responseJson) {
-            const key = item;
-            const value = responseJson[key];
-            displayJson += handleItem(key, value);
-        }
-        displayJson += '</div>';
-        return displayJson;
-    }
+    const parseObject = responseJson => {
+      let displayJson = '<div class="json">';
+      for (const item in responseJson) {
+        const key = item;
+        const value = responseJson[key];
+        displayJson += handleItem(key, value);
+      }
+      displayJson += '</div>';
+      return displayJson;
+    };
 
     this.refs.jsonviewer.innerHTML = parseObject(json);
   };
 
-  const json = this.opts.json;
   this.on('mount', () => {
-    updateJsonViewer();
-  }).on('updated', () => {
     updateJsonViewer();
   });
 }
