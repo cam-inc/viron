61c0e622a587cc9a1d06cc89726c76969c9fd699
Go Ohtani <cathcheeno@gmail.com>
2018-01-15 17:31:42 +0900

    Updated: theme for charts.

--- src/core/chart/themes/terminal.js
+++ src/core/chart/themes/terminal.js
@@ -2,7 +2,7 @@ export default {
   colors: ['#C092F5', '#61C8FF', '#7CD6AB', '#FFC96B', '#F57177', '#CBCFD4', '#484848'],
   legend: {
     itemStyle: {
-      color: '#484848'
+      color: '#35dcba'
     }
   }
 };
