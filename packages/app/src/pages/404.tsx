e815254ff5eefd30eb3b74ec445a48096da6cf4b
Go Ohtani <cathcheeno@gmail.com>
2021-01-08 14:14:48 +0900

    minimum gatsby

---
+++ packages/app/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = {};
+const NotfoundPage: React.FC<Props> = () => {
+  return (
+    <p>404</p>
+  );
+};
+
+export default NotfoundPage;
---
+++ packages/app/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = {};
+const IndexPage: React.FC<Props> = () => {
+  return (
+    <p>hello</p>
+  );
+};
+
+export default IndexPage;
