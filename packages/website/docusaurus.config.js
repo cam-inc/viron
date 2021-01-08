b4498714608cfd345d6f55f90ade2beeed980239
fkei <kei.topaz@gmail.com>
2021-01-08 17:34:59 +0900

    Fix: 🐛 document v2 skeleton

---
+++ packages/website/docusaurus.config.js
@@ -0,0 +1,94 @@
+module.exports = {
+  title: 'VIRON',
+  tagline: 'Automated admin tool',
+  url: 'https://github.com/cam-inc/viron',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'viron', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'VIRON',
+      logo: {
+        alt: 'VIRON',
+        src: 'img/logo.svg',
+      },
+      items: [{
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          to: 'blog',
+          label: 'Blog',
+          position: 'left'
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [{
+          title: 'Docs',
+          items: [{
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [{
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/viron',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [{
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} CAM, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ packages/website/src/pages/index.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'フロントレス',
+    imageUrl: 'img/frontend.png',
+    description: (
+      <>
+        HTML、CSS、JavaScript等のフロント用ソースコードを記述する必要はありません。APIサーバのみを用意して下さい。
+      </>
+    ),
+  },
+  {
+    title: 'OAS駆動',
+    imageUrl: 'img/oas.png',
+    description: (
+      <>
+        OpenAPI Specification 3.0に準拠。
+      </>
+    ),
+  },
+  {
+    title: '無料/オープンソース',
+    imageUrl: 'img/oss.png',
+    description: (
+      <>
+        GitHub上でオープンソースとして公開されています。誰でも無料で使用できます。
+      </>
+    ),
+  },
+  {
+    title: 'デスクトップアプリ提供',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        Chrome アプリを提供
+      </>
+    ),
+  },
+  {
+    title: 'レスポンシブデザイン',
+    imageUrl: 'img/responsive.png',
+    description: (
+      <>
+        デスクトップ端末とモバイル端末上で画面サイズに応じたレイアウトが設定されます。
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <p className="hero__subtitle">
+            All you do is just create a API server and a OAS2.0 json file. Then viron admin tool is ready to use. You don\'t need to write frontend code!
+          </p>
+          <div className={styles.buttons}>
+          <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
