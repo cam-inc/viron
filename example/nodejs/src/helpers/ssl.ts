ead820d3051f2ca4854536a2ceefb9041b66bd5b
Noritaka Okada <nori0123@gmail.com>
2021-05-28 17:08:14 +0900

    Fix: ssl for example

--- example/nodejs/src/helpers/ssl.ts
+++ example/nodejs/src/helpers/ssl.ts
@@ -4,12 +4,11 @@ import { SecureContextOptions } from 'tls';
 
 export const getCertificate = (): SecureContextOptions => {
   return {
-    // TODO: 正規のSSL証明書取得したら変更する
     cert: fs.readFileSync(
-      path.resolve(__dirname, '..', '..', 'cert', 'STAR_camplat_net.crt')
+      path.resolve(__dirname, '..', '..', 'cert', 'viron.crt')
     ),
     key: fs.readFileSync(
-      path.resolve(__dirname, '..', '..', 'cert', 'STAR_camplat_net.key')
+      path.resolve(__dirname, '..', '..', 'cert', 'viron.key')
     ),
   };
 };
