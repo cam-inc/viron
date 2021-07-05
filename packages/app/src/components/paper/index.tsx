051b662f8777c5a5ae9f3cab615126c3984a2639
Go Ohtani <cathcheeno@gmail.com>
2021-07-05 17:57:10 +0900

    Fix: purgable way.

--- packages/app/src/components/paper/index.tsx
+++ packages/app/src/components/paper/index.tsx
@@ -2,8 +2,8 @@ import classnames from 'classnames';
 import React from 'react';
 
 type Props = {
-  elevation: number;
-  shadowElevation: number;
+  elevation: 0 | 1 | 2 | 3 | 4 | 5;
+  shadowElevation: 0 | 1 | 2 | 3 | 4 | 5;
   square?: boolean;
 };
 const Paper: React.FC<Props> = ({
@@ -14,12 +14,23 @@ const Paper: React.FC<Props> = ({
 }) => {
   return (
     <div
-      className={classnames(
-        `text-on-surface bg-surface-0${elevation}dp shadow-0${shadowElevation}dp border border-on-surface-faint`,
-        {
-          rounded: !square,
-        }
-      )}
+      className={classnames(`text-on-surface border border-on-surface-faint`, {
+        // Avoid using string concatenation.
+        // @see: https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
+        'bg-surface-00dp': elevation === 0,
+        'bg-surface-01dp': elevation === 1,
+        'bg-surface-02dp': elevation === 2,
+        'bg-surface-03dp': elevation === 3,
+        'bg-surface-04dp': elevation === 4,
+        'bg-surface-05dp': elevation === 5,
+        'shadow-00dp': shadowElevation === 0,
+        'shadow-01dp': shadowElevation === 1,
+        'shadow-02dp': shadowElevation === 2,
+        'shadow-03dp': shadowElevation === 3,
+        'shadow-04dp': shadowElevation === 4,
+        'shadow-05dp': shadowElevation === 5,
+        rounded: !square,
+      })}
     >
       {children}
     </div>
