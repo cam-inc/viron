d1107a4367bf623bc45ff93103c9a2d2412e2f74
syama666 <yoshinori@gmail.com>
2021-07-26 20:07:55 +0900

    Modified: oas response api and ssl

--- example/golang/pkg/server/server.go
+++ example/golang/pkg/server/server.go
@@ -14,9 +14,18 @@ type Server struct {
 // Run server run
 func (s *Server) Run() error {
 	fmt.Println("INNER RUN")
-	addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
+	addr := fmt.Sprintf(":%d", s.Port)
+	//addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
 	fmt.Printf("Addr -> %s\n", addr)
 	return http.ListenAndServe(addr, s.handler)
+}
+
+func (s *Server) RunSSL() error {
+	fmt.Println("INNER RUN")
+	addr := fmt.Sprintf(":%d", s.Port)
+	//addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
+	fmt.Printf("Addr -> %s\n", addr)
+	return http.ListenAndServeTLS(addr, "/viron/example/golang/cert/viron.crt", "/viron/example/golang/cert/viron.key", s.handler)
 }
 
 // New start
