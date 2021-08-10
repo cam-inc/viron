8c491eed1bf4976407def808971331e86023506e
syama666 <yoshinori@gmail.com>
2021-08-10 15:14:05 +0900

    Add:

---
+++ packages/golang/domains/auth/signout.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"context"
+	"time"
+
+	"github.com/cam-inc/viron/packages/golang/repositories"
+
+	"github.com/cam-inc/viron/packages/golang/repositories/container"
+)
+
+/*
+// サインアウト - tokenを無効化
+export const signout = async (token?: string | null): Promise<boolean> => {
+  if (!token) {
+    return false;
+  }
+  const repository = repositoryContainer.getRevokedTokenRepository();
+  await repository.createOne({ token, revokedAt: new Date() });
+  debug('Signout token: %s', token);
+  return true;
+};
+
+// サインアウト済みか検証
+export const isSignedout = async (token?: string | null): Promise<boolean> => {
+  if (!token) {
+    return false;
+  }
+  const repository = repositoryContainer.getRevokedTokenRepository();
+  return !!(await repository.findOne({ token }));
+};
+*/
+
+func SignOut(ctx context.Context, token string) bool {
+	if token != "" {
+		return false
+	}
+
+	repo := container.GetRevokedTokensRepository()
+
+	revokedToken := &repositories.RevokedToken{
+		Token:     token,
+		RevokedAt: time.Now(),
+	}
+
+	if _, err := repo.CreateOne(ctx, revokedToken); err != nil {
+		return false
+	}
+
+	return true
+
+}
+
+func IsSignedOut(ctx context.Context, token string) bool {
+	if token != "" {
+		return false
+	}
+	repo := container.GetRevokedTokensRepository()
+	entity, err := repo.FindOne(ctx, token)
+	if err != nil {
+		return false
+	}
+	revoked := &repositories.RevokedToken{}
+	if err := entity.Bind(revoked); err != nil {
+		return false
+	}
+	return true
+}
