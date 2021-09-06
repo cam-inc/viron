5117e5c5ab3fa2f79cef7b0f38625adf697b8ff3
syama666 <yoshinori@gmail.com>
2021-09-06 20:01:46 +0900

    Add: example setup mongo connection

---
+++ example/golang/pkg/store/mongo.go
@@ -0,0 +1,44 @@
+package store
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/cam-inc/viron/example/golang/pkg/config"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type (
+	MongoClientWithOptions struct {
+		Client  *mongo.Client
+		Options *options.ClientOptions
+	}
+)
+
+var con *MongoClientWithOptions
+
+func SetupMongo(config *config.Mongo) {
+	opt := options.Client().ApplyURI(config.URI)
+	opt.SetAuth(options.Credential{
+		Username: config.User,
+		Password: config.Password,
+	})
+	client, err := mongo.NewClient(opt)
+	if err != nil {
+		panic(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	if err := client.Connect(ctx); err != nil {
+		panic(err)
+	}
+	con = &MongoClientWithOptions{
+		Client:  client,
+		Options: opt,
+	}
+}
+
+func GetMongoCollection() *MongoClientWithOptions {
+	return con
+}
