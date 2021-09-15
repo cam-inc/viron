bbdfe0ff5bb9d0a964ff7d7ef1ffd2f04b4c8b92
tomohiroyoshida <tomohiroyoshida0511@gmail.com>
2021-09-15 16:31:16 +0900

    small-fix

--- packages/app/src/components/description/index.tsx
+++ packages/app/src/components/description/index.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { CommonMark } from '$types/oas';
+import { CommonMark as CommonMarkType } from '$types/oas';
 import { ON, On } from '$constants/index';
 import { ClassName } from '$types/index';
 import classNames from 'classnames';
 
 type Props = {
   on: On;
-  value: CommonMark;
+  value: CommonMarkType;
   className?: ClassName;
 };
-const Description: React.FC<Props> = ({ value, on, className = '' }) => {
-  const md = require('markdown-it')('commonmark');
-  const result = (text: string) => {
-    return { __html: md.render(text) };
-  };
 
+const md = require('markdown-it')('commonmark');
+const getParsedResult = (commonMarkText: CommonMarkType) => {
+  return { __html: md.render(commonMarkText) };
+};
+const Description: React.FC<Props> = ({ value, on, className = '' }) => {
   return (
     <div
-      className={classNames('', className, {
+      className={classNames(className, {
         'text-on-background': on === ON.BACKGROUND,
         'text-on-surface': on === ON.SURFACE,
         'text-on-primary': on === ON.PRIMARY,
         'text-on-complementary': on === ON.COMPLEMENTARY,
       })}
-      dangerouslySetInnerHTML={result(value)}
+      dangerouslySetInnerHTML={getParsedResult(value)}
     />
   );
 };
