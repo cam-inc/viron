2d0032e31a60a9394a59a100bb28504c394945e3
tomohiroyoshida <tomohiroyoshida0511@gmail.com>
2021-09-15 16:09:25 +0900

    Add: Description component

--- packages/app/src/components/description/index.tsx
+++ packages/app/src/components/description/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { CommonMark } from '$types/oas';
+import { ON, On } from '$constants/index';
+import { ClassName } from '$types/index';
+import classNames from 'classnames';
 
 type Props = {
-  value: string | CommonMark;
+  on: On;
+  value: CommonMark;
+  className?: ClassName;
 };
-const Description: React.FC<Props> = ({ value }) => {
-  return <p>{value}</p>;
+const Description: React.FC<Props> = ({ value, on, className = '' }) => {
+  const md = require('markdown-it')('commonmark');
+  const result = (text: string) => {
+    return { __html: md.render(text) };
+  };
+
+  return (
+    <div
+      className={classNames('', className, {
+        'text-on-background': on === ON.BACKGROUND,
+        'text-on-surface': on === ON.SURFACE,
+        'text-on-primary': on === ON.PRIMARY,
+        'text-on-complementary': on === ON.COMPLEMENTARY,
+      })}
+      dangerouslySetInnerHTML={result(value)}
+    />
+  );
 };
+
 export default Description;
