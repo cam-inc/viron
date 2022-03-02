3b07dce0d1ff10b5900286edad88756575b51cbd
Go Ohtani <cathcheeno@gmail.com>
2022-03-02 11:31:12 +0900

    fix(website): link from top to get started page

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -29,7 +29,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-    to={useBaseUrl('docs/')}>
+    to={useBaseUrl('docs/introduction')}>
       {translate({
         message: 'Get Started'
       })}
