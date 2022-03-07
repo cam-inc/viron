37402aad953442b4d31d899c103be378ee2a9d31
Go Ohtani <cathcheeno@gmail.com>
2022-03-07 11:42:43 +0900

    chore(all): commitlint scope of intent added

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'version']],
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'intent', 'version']],
     'scope-enum': [2, 'always', ['all', 'example', 'app', 'linter', 'nodejs', 'website']],
   }
 };
