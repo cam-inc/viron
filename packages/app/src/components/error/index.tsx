150a13f8f34d87f85e5343dbd4baba178b8d0483
konotorrrri <konotorrrri29@gmail.com>
2024-04-05 16:14:47 +0900

    feat: modify error component

--- packages/app/src/components/error/index.tsx
+++ packages/app/src/components/error/index.tsx
@@ -2,7 +2,9 @@ import classnames from 'classnames';
 import React, { useEffect, useState } from 'react';
 import { Props as BaseProps } from '~/components';
 import ExclamationIcon from '~/components/icon/exclamation/outline';
+import InformationCircleIcon from '~/components/icon/informationCircle/outline';
 import { BaseError } from '~/errors';
+import { Trans, useTranslation } from '~/hooks/i18n';
 import Modal, { useModal } from '~/portals/modal/';
 import { error as logError, NAMESPACE } from '~/utils/logger';
 
@@ -10,7 +12,10 @@ type Props = BaseProps & {
   error?: BaseError;
   withModal?: boolean;
 };
-const Error: React.FC<Props> & { renewal: React.FC<Props> } = (props) => {
+const Error: React.FC<Props> & {
+  renewal: React.FC<Props>;
+  modal: React.FC<Props>;
+} = (props) => {
   const { error, withModal = false } = props;
 
   // log error.
@@ -31,7 +36,7 @@ const Error: React.FC<Props> & { renewal: React.FC<Props> } = (props) => {
       return;
     }
     modal.open();
-  }, [error, withModal, modal.open]);
+  }, [error, withModal, modal.open, modal]);
 
   return (
     <>
@@ -84,6 +89,38 @@ const Renewal: React.FC<Props> = (props) => {
 
 Error.renewal = Renewal;
 
+const ErrorModal: React.FC<Props> = (props) => {
+  const { error } = props;
+
+  // log error.
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+    logError({
+      messages: [error],
+      // TODO: namespaceを変えること。
+      namespace: NAMESPACE.REACT_COMPONENT,
+    });
+  }, [error]);
+
+  const modal = useModal();
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+    modal.open();
+  }, [error, modal, modal.open]);
+
+  return (
+    <Modal {...modal.bind}>
+      <_Error.modal {...props} />
+    </Modal>
+  );
+};
+
+Error.modal = ErrorModal;
+
 export default Error;
 
 export const useError = (
@@ -103,11 +140,10 @@ export const useError = (
   };
 };
 
-const _Error: React.FC<Props> & { renewal: React.FC<Props> } = ({
-  className = '',
-  on,
-  error,
-}) => {
+const _Error: React.FC<Props> & {
+  renewal: React.FC<Props>;
+  modal: React.FC<Props>;
+} = ({ className = '', on, error }) => {
   if (!error) {
     return null;
   }
@@ -178,3 +214,48 @@ const _ErrorRenewal: React.FC<Props> = ({ className = '', error }) => {
 };
 
 _Error.renewal = _ErrorRenewal;
+
+const _ErrorModal: React.FC<Props> = ({ className = '', on, error }) => {
+  const { t } = useTranslation();
+  if (!error) {
+    return null;
+  }
+
+  return (
+    <div className={classnames(`text-xs text-thm-on-${on}`, className)}>
+      <div className="flex flex-col">
+        <div
+          className={`flex items-center pb-3 gap-1 border-b border-thm-on-${on}-slight`}
+        >
+          <div className="flex-none">
+            <InformationCircleIcon className="w-6" />
+          </div>
+          <div className="flex-1 min-w-0">
+            <div className="text-lg font-bold text-thm-error">{error.name}</div>
+          </div>
+        </div>
+        <p className="mt-6 text-base text-thm-error">
+          <Trans
+            t={t}
+            i18nKey="fetchError"
+            components={{
+              br: <br />,
+            }}
+          />
+        </p>
+        {error.message && (
+          <>
+            <div className="mt-10 text-xs">{t('detail')}</div>
+            <textarea
+              className={`block w-full min-h-[160px] mt-2 p-3 border rounded resize-none border-thm-on-${on}-faint bg-thm-on-${on}-slight text-thm-on-${on} focus:outline-none`}
+              value={error.message}
+              readOnly
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+_Error.modal = _ErrorModal;
