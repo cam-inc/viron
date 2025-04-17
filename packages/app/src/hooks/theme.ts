376a82e042a42eb1a34a1bdb050454982dc0ee87
h8570rg <h8570rg@gmail.com>
2025-04-18 01:24:03 +0900

    fix: default theme

--- packages/app/src/hooks/theme.ts
+++ packages/app/src/hooks/theme.ts
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useAppThemeGlobalStateSet } from '~/store';
-import { Document, THEME } from '~/types/oas';
+import { Document } from '~/types/oas';
 
 export default (document: Document | null = null): void => {
   const setTheme = useAppThemeGlobalStateSet();
   useEffect(() => {
     if (document?.info['x-theme']) {
       setTheme(document.info['x-theme']);
+    } else {
+      setTheme('default');
     }
   }, [document, setTheme]);
 };
